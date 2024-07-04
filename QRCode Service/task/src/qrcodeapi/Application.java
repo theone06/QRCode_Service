87393e60aa151f5d6f40c17291c4f69ecc0b9432
theone06 <rnaps06@gmail.com>
2024-07-04 00:26:24 -0400

    Stage 4 of QRCode Service. Added 3rd party library to generate QRcodes

--- QRCode Service/task/src/qrcodeapi/Application.java
+++ QRCode Service/task/src/qrcodeapi/Application.java
@@ -1,5 +1,12 @@
 package qrcodeapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,24 +15,51 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
+
+   ObjectMapper objectMapper = new ObjectMapper();
+   String typeError = objectMapper.writeValueAsString(Map.of("error", "Only png, jpeg and gif" +
+           " image types are supported"));
+   String sizeError = objectMapper.writeValueAsString(Map.of("error", "Image size must be between " +
+           "150 and 350 pixels"));
+
+    public Application() throws JsonProcessingException {
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    public BufferedImage createQrCodeImage() {
-        BufferedImage image = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = image.createGraphics();
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 250, 250);
-        return image;
+    public BufferedImage createQrCodeImage(String data, int size) {
+        QRCodeWriter writer = new QRCodeWriter();
+        BufferedImage bufferedImage = null;
+        try {
+            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, size, size);
+            bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        } catch (WriterException e) {
+            System.out.println(e.getMessage());
+        }
+        return bufferedImage;
     }
+
+
+    public boolean isValidPixelSize(int input) {
+       return input >= 150 && input <= 350;
+    }
+
+    public boolean isValidMediaType(MediaType type) {
+        return type.equals(MediaType.IMAGE_PNG)
+                || type.equals(MediaType.IMAGE_JPEG)
+                || type.equals(MediaType.IMAGE_GIF);
+    }
+
 
     @RestController
     public class QrCodeController {
@@ -37,12 +71,29 @@ public class Application {
         }
 
         @GetMapping(path = "/api/qrcode")
-        public ResponseEntity<BufferedImage> getImage() {
-            BufferedImage bufferedImage = createQrCodeImage();
+        public ResponseEntity<?> getImage(@RequestParam(required = true) String contents, int size, @RequestParam(required = true) String type) throws JsonProcessingException {
+            MediaType mediaType = MediaType.parseMediaType("Image/" + type);
+            if (contents == null || contents.isBlank()) {
+                return ResponseEntity
+                        .badRequest()
+                        .body(objectMapper.writeValueAsString(Map.of("error", "Contents cannot be null or blank")));
+            }
+            if (isValidPixelSize(size)) {
+                if (isValidMediaType(mediaType)) {
+                    BufferedImage bufferedImage = createQrCodeImage(contents, size);
+                    return ResponseEntity
+                            .ok()
+                            .headers(httpHeaders -> httpHeaders.setContentType(mediaType))
+                            .body(bufferedImage);
+                } else {
+                    return ResponseEntity
+                            .badRequest()
+                            .body(typeError);
+                }
+            }
             return ResponseEntity
-                    .ok()
-                    .contentType(MediaType.IMAGE_PNG)
-                    .body(bufferedImage);
+                    .badRequest()
+                    .body(sizeError);
         }
     }
 
--- QRCode Service/task/test/QRCodeApiTest.java
+++ QRCode Service/task/test/QRCodeApiTest.java
@@ -5,11 +5,20 @@ import org.hyperskill.hstest.mocks.web.response.HttpResponse;
 import org.hyperskill.hstest.stage.SpringTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
+import static org.hyperskill.hstest.testing.expect.Expectation.expect;
+import static org.hyperskill.hstest.testing.expect.json.JsonChecker.isObject;
+import static org.hyperskill.hstest.testing.expect.json.JsonChecker.isString;
+
 public class QRCodeApiTest extends SpringTest {
+    private static final String BAD_SIZE_MSG = "Image size must be between 150 and 350 pixels";
+    private static final String BAD_TYPE_MSG = "Only png, jpeg and gif image types are supported";
+    private static final String BAD_CONTENTS_MSG = "Contents cannot be null or blank";
 
     CheckResult testGetHealth() {
         var url = "/api/health";
@@ -20,14 +29,14 @@ public class QRCodeApiTest extends SpringTest {
         return CheckResult.correct();
     }
 
-    CheckResult testGetQrCode() {
-        var url = "/api/qrcode";
+    CheckResult testGetQrCode(String contents, int size, String imgType, String expectedHash) {
+        var url = "/api/qrcode?contents=%s&size=%d&type=%s"
+                .formatted(encodeUrl(contents), size, imgType);
         HttpResponse response = get(url).send();
 
         checkStatusCode(response, 200);
-        checkContentType(response, "png");
+        checkContentType(response, imgType);
 
-        var expectedHash = "a370a8d3e1ee0f0184132a3c3b5d2952";
         var contentHash = getMD5Hash(response.getRawContent());
         if (!contentHash.equals(expectedHash)) {
             return CheckResult.wrong("""
@@ -44,10 +53,55 @@ public class QRCodeApiTest extends SpringTest {
         return CheckResult.correct();
     }
 
+    CheckResult testGetQrCodeInvalidParams(String contents, int size, String imgType, String message) {
+        var url = "/api/qrcode?contents=%s&size=%d&type=%s"
+                .formatted(encodeUrl(contents), size, imgType);
+
+        HttpResponse response = get(url).send();
+
+        checkStatusCode(response, 400);
+        checkErrorMessage(response, message);
+
+        return CheckResult.correct();
+    }
+
+    String[] contents = {
+            "text content",
+            "mailto:name@company.com",
+            "geo:-27.07,109.21",
+            "tel:1234567890",
+            "smsto:1234567890:texting!",
+            "Here is some text",
+            "https://hyperskill.org",
+            """
+            BEGIN:VCARD
+            VERSION:3.0
+            N:John Doe
+            ORG:FAANG
+            TITLE:CEO
+            TEL:1234567890
+            EMAIL:business@example.com
+            END:VCARD"""
+    };
+
     @DynamicTest
     DynamicTesting[] tests = {
             this::testGetHealth,
-            this::testGetQrCode
+
+            () -> testGetQrCode(contents[1], 200, "jpeg", "a9e1e394f5766304127ba88bd9f0bd5a"),
+            () -> testGetQrCode(contents[2], 200, "gif", "3d6cc8d84284c0d10af3370c1fa883a8"),
+            () -> testGetQrCode(contents[3], 300, "png", "e2e18076d34f09a01eb283c7b140b268"),
+            () -> testGetQrCode(contents[4], 300, "jpeg", "3f00dbd2593bdf4b229d6addf09464a4"),
+            () -> testGetQrCode(contents[5], 200, "gif", "db6ef9d4a2d81285c9f5ed85f870d092"),
+            () -> testGetQrCode(contents[6], 200, "jpeg", "401a4a780f22cd752b8162512d1eb3f8"),
+            () -> testGetQrCode(contents[7], 300, "gif", "d167d42b222297df6c754aea3273681f"),
+
+            () -> testGetQrCodeInvalidParams(contents[0], 99, "gif", BAD_SIZE_MSG),
+            () -> testGetQrCodeInvalidParams(contents[0], 351, "png", BAD_SIZE_MSG),
+            () -> testGetQrCodeInvalidParams(contents[0], 451, "webp", BAD_SIZE_MSG),
+            () -> testGetQrCodeInvalidParams(contents[0], 200, "webp", BAD_TYPE_MSG),
+            () -> testGetQrCodeInvalidParams("", 200, "webp", BAD_CONTENTS_MSG),
+            () -> testGetQrCodeInvalidParams("   ", 500, "webp", BAD_CONTENTS_MSG)
     };
 
     private void checkStatusCode(HttpResponse response, int expected) {
@@ -81,6 +135,24 @@ public class QRCodeApiTest extends SpringTest {
         }
     }
 
+    private void checkErrorMessage(HttpResponse response, String message) {
+        var endpoint = response.getRequest().getEndpoint();
+        if (!response.getJson().isJsonObject()) {
+            throw new WrongAnswer("""
+                    Request: GET %s
+                    
+                    Response contains a wrong object:
+                    Expected JSON but responded with %s
+                    
+                    """.formatted(endpoint, response.getContent().getClass())
+            );
+        }
+
+        expect(response.getContent()).asJson().check(
+                isObject().value("error", isString(message))
+        );
+    }
+
     private String getMD5Hash(byte[] rawContent) {
         try {
             var md = MessageDigest.getInstance("MD5");
@@ -93,5 +165,9 @@ public class QRCodeApiTest extends SpringTest {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String encodeUrl(String param) {
+        return URLEncoder.encode(param, StandardCharsets.UTF_8);
     }
 }
